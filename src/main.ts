cd5b2250edbe63b3596c693b1be0590bd04e5787
mtaliaf <marquis.taliaferro@gmail.com>
2016-08-06 13:31:58 -0500

    Initial Setup and toolbar

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
+import {disableDeprecatedForms, provideForms} from '@angular/forms'; 
+import { HTTP_PROVIDERS } from '@angular/http';
 import { AppComponent, environment } from './app/';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  HTTP_PROVIDERS,
+  disableDeprecatedForms(),
+  provideForms()
+])
+.catch((err: any) => console.error(err));
